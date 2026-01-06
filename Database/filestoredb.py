03d3f8d81c9bffbde9ebbb652db734bf5d26c0fb
sundar998 <sundar19921@gmail.com>
2026-01-06 19:33:12 +0530

    Update filestoredb.py

--- Database/filestoredb.py
+++ Database/filestoredb.py
@@ -1,1 +1,19 @@
+class FileStoreDB:
+    def __init__(self):
+        self.data = {}
+
+    async def create_store(self, store_id):
+        self.data[store_id] = []
+
+    async def add_file(self, store_id, msg_id):
+        self.data[store_id].append(msg_id)
+
+    async def get_files(self, store_id):
+        return self.data.get(store_id)
+
+    async def delete_store(self, store_id):
+        if store_id in self.data:
+            del self.data[store_id]
+
+fsdb = FileStoreDB()
 
